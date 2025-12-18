ee721e67ef63fd62db1ec6cd6d8939ce17e109fa
Abdullah Bawazeer <145490935+abawzr@users.noreply.github.com>
2025-12-19 01:46:03 +0300

    added player health

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,16 +1,77 @@
+using System;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float maxHealth = 100f;
+
+    private float _currentHealth;
+
+    public static event Action<float, float> OnHealthChanged; // (current, max)
+    public static event Action OnPlayerDeath;
+
+    private void Awake()
     {
-        
+        _currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        OnHealthChanged?.Invoke(_currentHealth, maxHealth);
+    }
+
+    private void Die()
+    {
+        OnPlayerDeath?.Invoke();
+
+        // Disable player controls
+        GetComponent<PlayerMovement>().enabled = false;
+        GetComponent<PlayerAttack>().enabled = false;
+        GetComponent<PlayerJump>().enabled = false;
+
+        // Play death animation
+        // GetComponent<Animator>().SetTrigger("Death");
+
+
+        // Respawn after delay
+        // Invoke("Respawn", 2f);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (_currentHealth <= 0) return;
+
+        _currentHealth -= damage;
+        _currentHealth = Mathf.Max(_currentHealth, 0); // Clamp to 0
+
+        OnHealthChanged?.Invoke(_currentHealth, maxHealth);
+
+        if (_currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        if (_currentHealth <= 0) return;
+
+        _currentHealth += amount;
+        _currentHealth = Mathf.Min(_currentHealth, maxHealth); // Clamp to max health
+
+        OnHealthChanged?.Invoke(_currentHealth, maxHealth);
+    }
+
+    public void Respawn(Vector3 spawnPosition)
+    {
+        _currentHealth = maxHealth;
+        transform.position = spawnPosition;
+
+        // Re-enable controls
+        GetComponent<PlayerMovement>().enabled = true;
+        GetComponent<PlayerAttack>().enabled = true;
+        GetComponent<PlayerJump>().enabled = true;
+
+        OnHealthChanged?.Invoke(_currentHealth, maxHealth);
     }
 }
